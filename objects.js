aa418986235befaf42089898baf92ab069faa8e4
Sean Barile <aquariussb84@gmail.com>
2019-04-03 00:53:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.songTitle = 'artistName'
+}
